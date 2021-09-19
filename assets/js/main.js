043d90426fdaed03839b8e8edd9d05a5574bc942
Alexandre Ferreira <Bd71409198*>
2021-09-19 20:29:23 +0100

    refactored the html, styles and script

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,110 +1,71 @@
-const quarter = window.prompt('Digite o numero do trimestre da planilha');
-const year = window.prompt('Digite o ano da planilha');
+(() => {
 
-const title = document.querySelector('h3');
+  const receive_form = document.getElementById('form-container')
 
-if (quarter != null && year != null && quarter != '' && year != '') {
-  title.innerHTML = `RECIBOS ${quarter}º TRIMESTRE DE ${year}`
-} else {
-  title.innerHTML = "Relatório Trimestral de IVA - Portugal"
+  //submit_btn = document.getElementById('form-btn')
+  let receives = []
+
+  receive_form.addEventListener('submit', function (evt) {
+    evt.preventDefault()
+
+    let receive_number = document.getElementById('receive_number').value
+    let client_name = document.getElementById('client-name').value
+    let receive_date = document.getElementById('receive-date').value
+    let value_no_taxes = document.getElementById('value-without-taxes').value
+    let value_iva = document.getElementById('value-iva').value
+
+    let idSetter = receives.length
+    inputVal = {
+      'id': idSetter++,
+      'receive': receive_number,
+      'client': client_name,
+      'date': receive_date,
+      'value': toCurrencyFormat(value_no_taxes),
+      'taxes': toCurrencyFormat(value_iva),
+    };
+
+    receives.push(inputVal);
+
+    renderReceives(receives)
+    clearInputVal()
+    
+  })
+
+})()
+
+function toCurrencyFormat(num) {
+  let val = new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR' }).format(num)
+  return val
 }
 
-var receivesLists = document.getElementById('receivesContainer');
+function renderReceives(receives) {
+  const template = document.querySelector('.table-row')
+  const table_content = document.getElementById('table-content')
+  table_content.innerHTML = ''
 
-var receiveNumber = document.querySelector('input#setReceive');
-var client = document.querySelector('input#setClient');
-var date = document.querySelector('input#setDate');
-var receiveValue = document.querySelector('input#setValue');
+  map = new Map(Object.entries(receives))
 
-var totalValue = [];
-var ivaValues = [];
-var irsValues = [];
+  map.forEach(function(rec) {
+    let item = document.createElement('tr')
+    item.id = rec.id
+    item.innerHTML = template.innerHTML
+    .replaceAll('{{ receive }}', rec.receive)
+    .replaceAll('{{ client }}', rec.client)
+    .replaceAll('{{ date }}', rec.date)
+    .replaceAll('{{ value-pure }}', rec.value)
+    .replaceAll('{{ value-iva }}', rec.taxes)
+    
+    item.classList.add('table-row', 'w-full', 'grid', 'grid-cols-6')
 
-var showTotal = document.querySelector('p.totalValue');
-var showTotalIVA = document.querySelector('p.totalIVA');
-var showTotalIRS = document.querySelector('p.totalIRS');
-
-function ivaCalc(v) {
-  return ((v * 0.23).toFixed(2));
+    table_content.appendChild(item)
+  })
+  // console.log(map)
+  
 }
 
-function irsCalc(v) {
-  return ((v * 0.25).toFixed(2));
+function clearInputVal() {
+  let all_inputs = document.querySelectorAll('input')
+  all_inputs.forEach(element => {
+    element.value = ''
+  });
 }
-
-function dateConvert(date) {
-  var datearray = date.split("-");
-  var newdate = datearray[2] + '/' + datearray[1] + '/' + datearray[0];
-  return newdate;
-}
-
-function adicionar() {
-
-  if (receiveNumber.value != '' && client.value != '' && date.value != '' && receiveValue.value != '') {
-
-    totalValue.push(receiveValue.value);
-    ivaValues.push(ivaCalc(receiveValue.value));
-    irsValues.push(irsCalc(receiveValue.value));
-
-    let line = document.createElement('div');
-
-    line.innerHTML = `<p class='receive'> ${receiveNumber.value} </p>
-    <p class='client'> ${client.value} </p>
-    <p class='date'> ${dateConvert(date.value)} </p>
-    <p class='value'> ${Number(receiveValue.value).toFixed(2)} €</p>
-    <p class='valueIVA'> ${ivaCalc(receiveValue.value)} €</p>
-    <p class='valueIRS'> ${irsCalc(receiveValue.value)} €</p>`;
-
-    receivesLists.appendChild(line);
-
-    let totalSum = 0;
-
-    for (let pos in totalValue) {
-      totalSum += Number(totalValue[pos]);
-    }
-
-    showTotal.innerHTML = `${(totalSum).toFixed(2)} €`;
-
-
-    let ivaTotalSum = 0;
-
-    for (let pos in ivaValues) {
-      ivaTotalSum += Number(ivaValues[pos]);
-    }
-
-    showTotalIVA.innerHTML = `${(ivaTotalSum).toFixed(2)} €`;
-
-
-    let irsTotalSum = 0;
-
-    for (let pos in irsValues) {
-      irsTotalSum += Number(irsValues[pos]);
-    }
-
-    showTotalIRS.innerHTML = `${(irsTotalSum).toFixed(2)} €`;
-
-    receiveNumber.value = '';
-    receiveNumber.focus();
-    client.value = '';
-    date.value = '';
-    receiveValue.value = '';
-  } else {
-    window.alert('É necessário preencher todos os campos.');
-  }
-};
-
-function addHR() {
-
-  let line = document.createElement('hr');
-
-  receivesLists.appendChild(line);
-
-};
-
-document.onkeydown = function (e) {
-  var key = e.key;
-
-  if (key == 'Enter') {
-    adicionar();
-  }
-};
